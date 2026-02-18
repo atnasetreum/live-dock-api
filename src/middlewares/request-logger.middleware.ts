105695e9b4ed1e8266dc2b4e6122b2645074bbcf
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-02-18 08:54:03 -0600

    feat: backup

--- src/middlewares/request-logger.middleware.ts
+++ src/middlewares/request-logger.middleware.ts
@@ -4,26 +4,26 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const requestId = Array.isArray(req.headers['x-request-id'])
+  use(_req: Request, _res: Response, next: NextFunction) {
+    /* const requestId = Array.isArray(req.headers['x-request-id'])
       ? req.headers['x-request-id'][0]
-      : (req.headers['x-request-id'] as string) || 'NO-ID';
+      : (req.headers['x-request-id'] as string) || 'NO-ID'; */
 
-    console.log({ requestId });
+    //console.log({ requestId });
 
     /* console.info(`[Backend] Request ${requestId}`, {
       method: req.method,
       url: req.originalUrl,
       body: (req.body as unknown) ?? null,
       timestamp: new Date().toISOString(),
-    }); */
+    });
 
     res.on('finish', () => {
-      /* console.info(`[Backend] Response ${requestId}`, {
+      console.info(`[Backend] Response ${requestId}`, {
         statusCode: res.statusCode,
         timestamp: new Date().toISOString(),
-      }); */
-    });
+      });
+    }); */
 
     next();
   }
